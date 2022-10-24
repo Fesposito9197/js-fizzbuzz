4a9858c522863dcf287ee7434394fda0d50e0b14
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-24 15:52:56 +0200

    aggiunto tabella in html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
+
+
+const cellContainer = document.querySelector(".cell-container");
+
+
+
+
 for( let i = 1 ; i <= 100; i++){
+    const numberCell = document.createElement("div");
+    numberCell.className = "number-cell";
     if( i % 3===0 && i % 5 ===0){
-        console.log("Fizzbuzz")
+        numberCell.classList.add('number-cell-fizz-buzz')
+        numberCell.innerHTML = "FizzBuzz"
+       
     } else if( i % 3 ===0){
+        numberCell.innerHTML = "Fizz"
+        numberCell.classList.add('number-cell-fizz')
         console.log("Fizz")
     } else if( i % 5 ===0){
+        numberCell.innerHTML = "Buzz"
+        numberCell.classList.add('numbner-cell-buzz')
         console.log('Buzz')
     }else{
+        numberCell.innerHTML = i
+
         console.log(i)
     }
+    cellContainer.append(numberCell)
+    
 }
